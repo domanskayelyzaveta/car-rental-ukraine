7b7f311324297b7defcf7141f4185f4644c06089
domanskayelyzaveta <domanskaya_@ukr.net>
2023-11-14 11:39:59 +0100

    loader fixed

--- src/pages/Catalog/CatalogPage.jsx
+++ src/pages/Catalog/CatalogPage.jsx
@@ -2,24 +2,14 @@ import React from "react";
 import CatalogList from "../../components/CatalogList/CatalogList";
 import { CatalogListWrapper } from "./CatalogPage.styled";
 import FormSelectors from "../../components/FromSelectors/FormSelectors";
-import Loader from "../../components/Loader/Loader";
-import { useSelector } from "react-redux";
-import { selectIsLoading } from "../../Redux/selectors";
 
 const Catalog = () => {
-  const isLoading = useSelector(selectIsLoading);
   return (
     <div>
-      {isLoading ? (
-        <Loader />
-      ) : (
-        <>
-          <FormSelectors />
-          <CatalogListWrapper>
-            <CatalogList />
-          </CatalogListWrapper>
-        </>
-      )}
+      <FormSelectors />
+      <CatalogListWrapper>
+        <CatalogList />
+      </CatalogListWrapper>
     </div>
   );
 };
