ca068f0f91990dfe11105df336818af395e875d5
domanskayelyzaveta <domanskaya_@ukr.net>
2023-11-14 09:22:44 +0100

    button load more fixed

--- src/components/ButtonLoadMore/ButtonLoadMore.jsx
+++ src/components/ButtonLoadMore/ButtonLoadMore.jsx
@@ -2,16 +2,11 @@ import React, { useState } from "react";
 import { ButtonWrapper, StyledLoadMoreButton } from "./ButtonLoadMore.styled";
 import { useDispatch, useSelector } from "react-redux";
 import { getCarThunk } from "../../Redux/thunks";
-import {
-  selectCarsData,
-  selectHasMorePages,
-  selectsFilteredMake,
-} from "../../Redux/selectors";
+import { selectHasMorePages, selectsFilteredMake } from "../../Redux/selectors";
 
 const ButtonLoadMore = () => {
   const dispatch = useDispatch();
   const hasMorePages = useSelector(selectHasMorePages);
-  const CarsData = useSelector(selectCarsData);
   const Filtered = useSelector(selectsFilteredMake);
   const [page, setPage] = useState(2);
 
@@ -20,8 +15,7 @@ const ButtonLoadMore = () => {
     setPage(page + 1);
   };
 
-  const shouldHideButton =
-    !hasMorePages && CarsData.length <= 0 && Filtered.length <= 0;
+  const shouldHideButton = !hasMorePages || Filtered.length <= 0;
 
   return (
     <ButtonWrapper>
